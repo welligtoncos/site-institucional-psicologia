c31ea7e4f4c17957bf92c874c59df0067b89ac94
welligtoncos <welligtoncos@gmail.com>
2026-04-26 22:58:10 -0300

    chore(seo): remove diretiva Host do robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,5 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
     sitemap: "https://psicologoonlineja.com/sitemap.xml",
-    host: "https://psicologoonlineja.com",
   };
 }
